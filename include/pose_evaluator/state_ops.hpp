98ee5a3147b8226843f0dd1ae12a383c54f1c6a4
andrey2605 <andrey26052001@gmail.com>
2026-04-21 19:26:03 +0000

    update ukf

--- include/pose_evaluator/state_ops.hpp
+++ include/pose_evaluator/state_ops.hpp
@@ -10,20 +10,33 @@ struct StateOps
   static State boxPlus(const State & x, const ErrorVec & dx)
   {
     State out = x;
+
+    // Позиция и скорости обновляются обычным сложением
     out.p += dx.segment<3>(0);
     out.v += dx.segment<3>(3);
-    out.q = (x.q * quatExp(dx.segment<3>(6))).normalized();
     out.w += dx.segment<3>(9);
+
+    // Левое приращение ориентации:
+    // q_new = Exp(dtheta) * q
+    const Eigen::Vector3d dtheta = dx.segment<3>(6);
+    out.q = (quatExp(dtheta) * x.q).normalized();
+
     return out;
   }
 
   static ErrorVec boxMinus(const State & a, const State & b)
   {
     ErrorVec dx;
+
     dx.segment<3>(0) = a.p - b.p;
     dx.segment<3>(3) = a.v - b.v;
-    dx.segment<3>(6) = quatLog(b.q.conjugate() * a.q);
     dx.segment<3>(9) = a.w - b.w;
+
+    // При левом приращении относительная ошибка ориентации должна быть:
+    // dq = a.q * b.q^{-1}
+    const Eigen::Quaterniond dq = a.q * b.q.conjugate();
+    dx.segment<3>(6) = quatLog(dq);
+
     return dx;
   }
 };
